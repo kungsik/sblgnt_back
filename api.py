67762e3a1948a117b33b6730c2e45b24721b2161
kungsik <kungsik@hotmail.com>
2019-08-10 15:14:15 +0900

    modifying the translate

--- api.py
+++ api.py
@@ -5,18 +5,18 @@ api response정의
 '''
 
 from flask import Blueprint, jsonify, request
-from sblgnt_back.controller import translateVersion as tv
+#from sblgnt_back.controller import translateVersion as tv
 from sblgnt_back.controller import gntVersion as gv
 
 
 api = Blueprint('api', __name__)
 
 # 한글, 영어 번역본 텍스트 불러오기
-@api.route('/text/<string:ver>/<string:bib>/<string:book>/<string:chp>/')
-def translateVersion(ver, book, chp, bib):
-    result = tv.json_to_verse(ver, book, chp, bib)
-    verse = {'verse': result}
-    return jsonify(verse)  
+#@api.route('/text/<string:ver>/<string:bib>/<string:book>/<string:chp>/')
+#def translateVersion(ver, book, chp, bib):
+#    result = tv.json_to_verse(ver, book, chp, bib)
+#    verse = {'verse': result}
+#    return jsonify(verse)  
 
 # gnt 텍스트 불러오기
 @api.route('/text/gnt/<string:book>/<int:chp>/')
--- controller/gntVersion.py
+++ controller/gntVersion.py
@@ -10,6 +10,10 @@ C:/Users/kungs/Dropbox/text-fabric-data/sblgnt
 '''
 
 from tf.fabric import Fabric
+import os
+import json
+import codecs
+from flask import request
 
 SBLGNT = 'sblgnt'
 TG = Fabric( modules=SBLGNT, silent=False )
@@ -24,11 +28,44 @@ gnt = TG.load('''
 ''')
 # 보류: ClType, function
 
+# 번역본 텍스트 불러오기
+#자체 json 파일로 번역본 인용
+def json_to_verse(ver, book, chp, bib):
+    path = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+
+    if bib == "old" :
+        book_code = {
+            "Genesis": 0, "Exodus": 1, "Leviticus": 2, "Numbers": 3, "Deuteronomy": 4, "Joshua": 5, "Judges": 6, "Ruth": 7,
+            "1_Samuel": 8, "2_Samuel": 9, "1_Kings": 10, "2_Kings": 11, "1_Chronicles": 12, "2_Chronicles": 13, "Ezra": 14, "Nehemiah": 15,
+            "Esther": 16, "Job": 17, "Psalms": 18, "Proverbs": 19, "Ecclesiastes": 20, "Song_of_songs": 21, "Isaiah": 22, "Jeremiah": 23, "Lamentations": 24,
+            "Ezekiel": 25, "Daniel": 26, "Hosea": 27, "Joel": 28, "Amos": 29, "Obadiah": 30, "Jonah": 31, "Micah": 32, "Nahum": 33, "Habakkuk": 34,
+            "Zephaniah": 35, "Haggai": 36, "Zechariah": 37, "Malachi": 38
+        }
+        location = path + "/static/version/" + ver + "_old.json"
+    else :
+        book_code = {
+            "Matthew": 0, "Mark": 1, "Luke": 2, "John": 3, "Acts": 4, "Romans": 5, "1_Corinthians": 6, "2_Corinthians": 7,
+            "Galatians": 8, "Ephesians": 9, "Philippians": 10, "Colossians": 11, "1_Thessalonians": 12, "2_Thessalonians": 13,
+            "1_Timothy": 14, "2_Timothy": 15, "Titus": 16, "Philemon": 17, "Hebrews": 18, "James": 19, "1_Peter": 20,
+            "2_Peter": 21, "1_John": 22, "2_John": 23, "3_John":24, "Jude": 25, "Revelation": 26
+        }
+        location = path + "/static/version/" + ver + "_new.json"
+
+    with codecs.open(location, 'r', 'utf-8-sig') as json_data:
+        d = json.load(json_data)
+        json_chp = int(chp) - 1
+        try:
+            verse_str = d[book_code[book]]['chapters'][json_chp][chp]
+            return verse_str
+        except KeyError:
+            return 'DB 오류로 번역지원되지 않음.'
+
+
+
 # 그리스어 텍스트 불러오기
 def getGnt(book='Matthew', chapter=1):
     chpNode = gnt.T.nodeFromSection((book, chapter))
     verseNode = gnt.L.d(chpNode, otype='verse')
-
     # verse = '''
     #     <div ref='verse'>
     #     <div style="text-align:left">
@@ -36,8 +73,13 @@ def getGnt(book='Matthew', chapter=1):
     #     </div>
     #     <ol>
     # '''
-
     verse = '<ol>'
+
+    #한글 번역본 로드(신약)
+    korVrs = json_to_verse('kor', book, chapter, 'new')
+    print(korVrs)
+    i = 0;
+
     for v in verseNode:
         verse += '<li>'
         wordsNode = gnt.L.d(v, otype='word')
@@ -70,9 +112,15 @@ def getGnt(book='Matthew', chapter=1):
                 verse += '<span class=trailerNode>'
                 verse += gnt.F.trailer.v(w)
                 verse += '</span>'
-
+            
             # if w == lastClauseWordNode: verse += '</span>'
             # if w == lastPhraseWordNode: verse += '</span>'
+
+            #한글 구절 추가
+            #print(korVrs)
+            #i = i + 1
+            #verseNum = str(i)
+            #verse += '<p>' + korVrs[verseNum] + '</p>'
 
         ## span end태그 오류가 생길 경우(신택스 뷰어 설정시) 아래와 같이 조정하면 고쳐짐. 
         # verse += '</span></span></span></span></li>'
